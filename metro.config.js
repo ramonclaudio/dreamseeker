ebf60afe5e394858a13f11cb63748a442ccefe09
Ray <hello@ramonclaudio.com>
2025-12-31 19:25:03 -0500

    feat(metro): add production bundle optimizations
    - Add drop_console to remove console.log/info in production
    - Enable experimentalImportSupport for better tree shaking
    - Enable inlineRequires for faster app startup

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,20 @@ const { withNativewind } = require('nativewind/metro');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Production optimizations
+config.transformer.minifierConfig = {
+  compress: {
+    // Remove console logs in production
+    drop_console: ['log', 'info'],
+  },
+};
+
+// Enable inline requires for faster startup (safe with tree shaking)
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: true,
+    inlineRequires: true,
+  },
+});
+
 module.exports = withNativewind(config);
