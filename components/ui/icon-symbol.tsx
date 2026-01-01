41298761dff3a3b207e5bb353b82e453c3c03cf5
Ray <hello@ramonclaudio.com>
2026-01-01 10:55:08 -0500

    chore: remove orphaned comment block

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -15,12 +15,6 @@ const MAPPING = {
 type IconSymbolName = keyof typeof MAPPING;
 
 /**
- * Add your SF Symbols to Material Icons mappings here.
- * - see Material Icons in the [Icons Directory](https://icons.expo.fyi).
- * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app.
- */
-
-/**
  * An icon component that uses native SF Symbols on iOS, and Material Icons on Android and web.
  * This ensures a consistent look across platforms, and optimal resource usage.
  * Icon `name`s are based on SF Symbols and require manual mapping to Material Icons.
