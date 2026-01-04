bd86d91486d3dca2b8ffa50f54099b04f41deae4
Ray <hello@ramonclaudio.com>
2026-01-04 09:03:53 -0500

    feat(components): add className prop to ThemedText and ThemedView

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -5,10 +5,19 @@ import { useThemeColor } from '@/hooks/use-theme-color';
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  className?: string;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+export function ThemedView({
+  style,
+  className,
+  lightColor,
+  darkColor,
+  ...otherProps
+}: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  return (
+    <View className={className} style={[{ backgroundColor }, style]} {...otherProps} />
+  );
 }
