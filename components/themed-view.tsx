3db22ebb40475f8057a0b1ce55b439ee70eaf400
Ray <hello@ramonclaudio.com>
2026-01-08 20:25:04 -0500

    refactor(components): inline types and compress styles

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -1,23 +1,9 @@
 import { View, type ViewProps } from 'react-native';
-
 import { useThemeColor } from '@/hooks/use-theme-color';
 
-export type ThemedViewProps = ViewProps & {
-  lightColor?: string;
-  darkColor?: string;
-  className?: string;
-};
+export type ThemedViewProps = ViewProps & { lightColor?: string; darkColor?: string; className?: string };
 
-export function ThemedView({
-  style,
-  className,
-  lightColor,
-  darkColor,
-  ...otherProps
-}: ThemedViewProps) {
+export function ThemedView({ style, className, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
-
-  return (
-    <View className={className} style={[{ backgroundColor }, style]} {...otherProps} />
-  );
+  return <View className={className} style={[{ backgroundColor }, style]} {...otherProps} />;
 }
