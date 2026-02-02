cbb675736698b1232a5e7a4c65a07ea4db9c45e8
Ray <hello@ramonclaudio.com>
2026-02-02 18:40:36 -0500

    refactor(schema): remove unused userFiles table

--- convex/schema.ts
+++ convex/schema.ts
@@ -11,14 +11,6 @@ export default defineSchema({
     .index('by_user', ['userId'])
     .index('by_user_completed', ['userId', 'isCompleted']),
 
-  userFiles: defineTable({
-    userId: v.string(),
-    storageId: v.string(),
-    createdAt: v.number(),
-  })
-    .index('by_user', ['userId'])
-    .index('by_storage_id', ['storageId']),
-
   pushTokens: defineTable({
     userId: v.string(),
     token: v.string(),
